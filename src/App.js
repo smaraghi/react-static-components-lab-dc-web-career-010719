6a76e084590e71a8da642c84d1c25fe06a5c8dd9
Serven Maraghi <maraghiserven@gmail.com>
2019-03-09 19:02:58 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
 	render() {
 		// your code in the return statement below!
 		return (
-			<div className="App">
+			<div>
+				<CatComponent />
+				<EinsteinQuoteComponent />
+				<MouseComponent />
 			</div>
 		);
 	}
